dff174b1e31aacfbbe945eac26069900910f213e
karnedo <karnedo@proton.me>
2024-01-26 10:43:51 +0100

    started working on Themes

---
+++ app/src/main/java/com/example/wanderwisely/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.example.wanderwisely.ui.theme
+
+import androidx.compose.ui.graphics.Color
+/*
+val secondaryDark = Color(0xFF6A994E)
+val primaryDark = Color(0xFF386641)
+
+val primaryWhite = Color(0xFF6A994E)
+val secondaryWhite = Color(0xFFA7C957)*/
+
+
+val primaryDark = Color(0xFF386641)
+val secondaryDark = Color(0xFF6A994E)
+val onPrimaryDark = Color.White
+
+val primaryWhite = Color(0xFFA7C957)
+val secondaryWhite = Color(0xFFC6E48B)
+val onSurfaceWhite = Color.Black
+
+val backgroundDark = Color(0xFF1E2C16)
+val surfaceDark = Color(0xFF23301B)
+val onBackgroundDark = Color.White
+val onSurfaceDark = Color.White
